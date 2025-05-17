7b31100d7df11e114453f75497ef96ac510bf5ee
Jasper Holton <jasper.camber.holton@gmail.com>
2025-05-17 00:04:57 -0700

    open source

--- app/views.py
+++ app/views.py
@@ -6,15 +6,26 @@ from django.views.decorators.vary import vary_on_cookie
 
 # Create your views here.
 #@cache_control(public=True)
-@cache_page(60*60*24*7)
-@vary_on_cookie
+#@cache_page(60*60*24*7)
+#@vary_on_cookie
+@never_cache
 def app(request):
     from django.conf import settings
 #    r = render(request, 'app/app.html', {'title': 'App', 'hidenavbar': True, 'full': True, 'nopadding': True, 'default_page': settings.DEFAULT_PAGE, 'hiderrm': False, 'no_overscroll': True})
     from django.http import HttpResponseRedirect
     from django.urls import reverse
     from django.shortcuts import redirect
+    import datetime
     r = None
+    if not request.user.is_authenticated and not request.COOKIES.get('guest_visit'):
+        max_age = settings.PUSH_COOKIE_EXPIRATION_HOURS * 60 * 60
+        expires = datetime.datetime.strftime(
+        datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),
+            "%a, %d-%b-%Y %H:%M:%S GMT",
+        )
+        response = redirect('landing:index')
+        response.set_cookie('guest_visit', True, max_age=max_age, expires=expires)
+        return response
     if request.user.is_authenticated and request.user.profile.vendor:
         r = redirect(reverse('go:go'))
     else:
@@ -24,3 +35,4 @@ def app(request):
     if request.user.is_authenticated and request.user.profile.vendor: patch_cache_control(r, private=True)
     else: patch_cache_control(r, public=True)
     return r
+
